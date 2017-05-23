2ca386f3f1289de4db49415b6ec63c400d15696e
rodrigopk <rodrigopk>
2017-05-22 22:13:10 -0300

    adding unit tests and adding case for finding prices when it does not exists on one list

--- telephone.rb
+++ telephone.rb
@@ -29,20 +29,40 @@ class Telephone
   end
 
   def get_smaller_price(number)
-    @operators.map(&:rate_for_number.with(number)).min
+    @operators      
+      .map(&:rate_for_number.with(number))
+      .compact
+      .min
   end
 
 end
 
-op_a = Operator.new("A",{"123" => 0.5, "12345" => 0.11, "1234" => 0.43, "321" => 0.43})
-op_b = Operator.new("B",{"12" => 0.6, "1235" => 0.21, "1234" => 0.53})
-tel = Telephone.new([op_a,op_b])
-number = '123457890020'
+op_a = Operator.new("A",{
+                          "1" =>  0.9, 
+                          "268" => 5.1, 
+                          "46" => 0.17, 
+                          "4620" => 0.0, 
+                          "468" => 0.15, 
+                          "4631" => 0.15, 
+                          "4673" => 0.9, 
+                          "46732" => 1.1
+                        })
 
-puts("#: #{number}")
-puts("#OP_A {op_a.name} price list: #{op_a.price_list}")
-puts("#OP_B {op_b.name} price list: #{op_b.price_list}")
-puts("\n")
-puts("A rate: #{op_a.rate_for_number(number)}")
-puts("B rate: #{op_b.rate_for_number(number)}")
-puts("lowest: #{tel.get_smaller_price(number)}")
+op_b = Operator.new("B",{
+                          "1" => 0.92, 
+                          "44" => 0.5, 
+                          "46" => 0.2,
+                          "467" => 1.0,
+                          "48" => 1.2
+                        })
+
+tel = Telephone.new([op_a,op_b])
+# number = '4473212345'
+
+# puts("#: #{number}")
+# puts("#OP_A {op_a.name} price list: #{op_a.price_list}")
+# puts("#OP_B {op_b.name} price list: #{op_b.price_list}")
+# puts("\n")
+# puts("A rate: #{op_a.rate_for_number(number)}")
+# puts("B rate: #{op_b.rate_for_number(number)}")
+# puts("lowest: #{tel.get_smaller_price(number)}")
---
+++ telephone_test.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'telephone'
+
+class TelephoneTest < Minitest::Test
+
+  def setup
+    op_a = Operator.new("A",{
+                          "1" =>  0.9, 
+                          "268" => 5.1, 
+                          "46" => 0.17, 
+                          "4620" => 0.0, 
+                          "468" => 0.15, 
+                          "4631" => 0.15, 
+                          "4673" => 0.9, 
+                          "46732" => 1.1
+                        })
+
+  op_b = Operator.new("B",{
+                            "1" => 0.92, 
+                            "44" => 0.5, 
+                            "46" => 0.2,
+                            "467" => 1.0,
+                            "48" => 1.2
+                          })
+  
+  @tel = Telephone.new([op_a,op_b])
+  end
+
+  def test_find_smallest_rate_from_operators
+    number = '4673212345'
+    
+    assert_equal 1.0, @tel.get_smaller_price(number)
+  end
+
+  def test_find_price_that_doesnt_exists_in_one_list
+    number = '4473212345'
+    
+    assert_equal 0.5, @tel.get_smaller_price(number)
+  end
+
+end
